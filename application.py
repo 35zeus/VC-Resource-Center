709ecb9f90dc0863d6acb9d69d7fcac0ca45f861
35Zeus <benn.andrews2022@gmail.com>
2023-09-26 10:53:35 -0700

    Added comments and restruct

--- application.py
+++ application.py
@@ -10,11 +10,12 @@ application.secret_key = os.urandom(12).hex()
 
 year = date.today().year
 today = date.today()
-events_json = requests.get('https://api.npoint.io/a0fbacdbf8f5e6a731a3').json()
 
 load_dotenv()
 
 
+# The following function takes env variables of the moderators email credentials, formats an email to the correct
+# recipient and sends the email.
 def email_message(name, contact_email, body='New Sign up for the newsletter', category='Newsletter'):
     sending_email = os.environ.get('MOD_EMAIL')
     sending_password = os.environ.get('MOD_EMAIL_PASSWORD')
@@ -31,8 +32,10 @@ def email_message(name, contact_email, body='New Sign up for the newsletter', ca
         )
 
 
+# renders the landing page with the events data. It will exclude all events that precede the current date.
 @application.route('/')
 def landing():
+    events_json = requests.get('https://api.npoint.io/a0fbacdbf8f5e6a731a3').json()
     for x in range(len(events_json)):
         my_date = events_json[x]["date"]
         date_object = datetime.strptime(my_date, '%Y-%m-%d').date()
@@ -50,11 +53,13 @@ def get_contact_page():
     return render_template("contact.html", current_year=year)
 
 
+# Adds string stating the contact form sending was successful (since I don't know JS yet)
 @application.route('/contact-success')
 def get_contact_success_page():
     return render_template('contact-success.html')
 
 
+# Takes in contact form data from contact.html to send to administrator's email
 @application.route('/submit', methods=['POST'])
 def get_form_sent():
     if request.method == 'POST':
@@ -62,27 +67,31 @@ def get_form_sent():
         contact_email = request.form['contact-email']
         category = request.form['category']
         message = request.form['message']
-        email_message(contact_name, contact_email, category, message)
+        email_message(contact_name, contact_email, message, category)
     return application.redirect('/contact-success')
 
 
+# a route for the footer on any html page to go to in order to send a notification that someone has signed up for the
+# newsletter.
 @application.route('/sign-up', methods=['POST', 'GET'])
 def newsletter_signup():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         email_message(name, email)
-    return render_template('redirect.html')
+    return render_template('index.html')
 
 
+# gets the post selected data and displays it as a full page
 @application.route('/post')
 def get_full_post():
+    events_json = requests.get('https://api.npoint.io/a0fbacdbf8f5e6a731a3').json()
     id_num = int(request.args["id_num"])
     post_data: dict = {}
     for post in events_json:
         if id_num == post["id"]:
             post_data = post
-    return render_template('full-post.html', post_data=post_data)
+    return render_template('full-post.html', post_data=post_data, current_year=year)
 
 
 if __name__ == "__main__":
